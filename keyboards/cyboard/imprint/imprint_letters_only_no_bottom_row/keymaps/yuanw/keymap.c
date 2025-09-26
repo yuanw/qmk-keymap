0a22a087bcb89cd42c4d6ca2b2f69eb785c10abd
yuanwang <yuan.wang@workiva.com>
2025-09-26 13:06:29 -0600

    this build

--- keyboards/cyboard/imprint/imprint_letters_only_no_bottom_row/keymaps/yuanw/keymap.c
+++ keyboards/cyboard/imprint/imprint_letters_only_no_bottom_row/keymaps/yuanw/keymap.c
@@ -8,7 +8,6 @@
 #ifdef OS_DETECTION_ENABLE
 #    include "os_detection.h"
 #endif
-#include "unicode.h"
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -18,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                             KC_ENT,  KC_DEL,  KC_ESC,           KC_GRAVE, KC_BSPC, KC_SPC,
-                                            KC_LGUI, KC_HOME, KC_END,           KC_CAPS,  KC_APP,  MO(1)
+                                            QK_BOOT, KC_HOME, KC_END,           KC_CAPS,  KC_APP,  MO(1)
     ),
 
     [1] = LAYOUT_let_no_bottom_row(
