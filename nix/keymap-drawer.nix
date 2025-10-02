848d4616cc228c45a2eed69286b16f55d8e7c5ce
yuanw <me@yuanwang.ca>
2025-10-01 21:07:43 -0600

    replace sha256

--- nix/keymap-drawer.nix
+++ nix/keymap-drawer.nix
@@ -25,11 +25,8 @@ buildPythonApplication rec {
     owner = "caksoylar";
     repo = pname;
     rev = "f9cf9d3677368a7fb1dc505c51b8156fafa66fe2";
-    sha256 = lib.fakeSha256;
-    #sha256 = "sha256-EfFCuqL2/eMUweMip+YUa4iCZouqKC3PbiKRWcLxCvg=";
-    # rev = "855933863fcc6f0db6098a03e679319dbf7f8bf2";
-    #                      sha256 = lib.fakeSha256;
-    #hash = "sha256-InddS9NxVrYOufiP7iWQTQ3VBeJgX2UlBA+Gf7ZfFrI=";
+    #sha256 = lib.fakeSha256;
+    sha256 = "sha256-SPnIfrUA0M9xznjEe60T+0VHh9lCmY4cni9hyqFlZqM=";
   };
 
   build-system = [ poetry-core ];
